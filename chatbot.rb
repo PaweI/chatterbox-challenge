20ce75898fde3ab7b480e28769ab0c76c79a551a
Pavel Redics <pavel.redics@gmail.com>
2014-10-26 21:13:31 +0000

    Added 5 more responses

--- chatbot.rb
+++ chatbot.rb
@@ -9,7 +9,13 @@ RESPONSES = { 'goodbye' => 'bye',
               'sayonara' => 'sayonara', 
               'the weather is (.*)' => 'I hate it when it\'s %{c1}', 
               'I love (.*)' => 'I love %{c1} too', 
-              'I groove to (.*) and (.*)' => 'I love %{c1} but I hate %{c2}'}
+              'I groove to (.*) and (.*)' => 'I love %{c1} but I hate %{c2}',
+              'How are you' => 'I am very good, thank you!',
+              'Did you try (.*)' => 'Yes, I like %{c1}',
+              'Who is your creator?' => 'Internet wisdom',
+              'Have you seen movie (.*)' => 'Oh, %{c1}. I watched it today. Could be better',
+              'Which OS better (.*) or (.*)' => 'Of course %{c1}! But %{c2} have some advantages too :-)'}
+
 
 puts "Hello, what's your name?"
 name = gets.chomp
